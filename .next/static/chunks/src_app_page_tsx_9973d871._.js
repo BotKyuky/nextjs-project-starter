8f8c68a807646c40cd28c5642613295574023f1b
BotKyuky <167480310+BotKyuky@users.noreply.github.com>
2025-07-18 14:18:41 +0200

    Add .next/static/chunks/src_app_page_tsx_9973d871._.js 1752841120869

---
+++ .next/static/chunks/src_app_page_tsx_9973d871._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_cee28d55._.js"
+],
+    source: "dynamic"
+});
